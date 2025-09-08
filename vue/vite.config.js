293283e6696856b376e3eb94654388fd4d5f05a3
Jesús Daniel Gómez Hernández <psp.dgomez@centrogeo.edu.mx>
2025-09-08 15:35:30 -0600

    [vue] Creando vis del metro

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -1,23 +1,21 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
+import dsv from "@rollup/plugin-dsv";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), dsv()],
   server: {
-    host: '0.0.0.0',
-    allowedHosts:[""],
+    host: "0.0.0.0",
+    allowedHosts: [""],
     port: 5174,
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
