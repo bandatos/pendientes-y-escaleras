8950a29e101603487ac72da64f1edd035db1b919
Seldon <daanmaro99june@gmail.com>
2025-10-06 18:31:24 -0600

    [vue] feat(stationStore): implement store for managing station catalog and selection feat(surveyStore): create store for handling survey progress and stair data management refactor(index.js): organize store exports for better accessibility

--- vue/src/stores/index.js
+++ vue/src/stores/index.js
@@ -1,3 +1,10 @@
 import { createPinia } from 'pinia'
-//Creación de pinia, para los stores.
+
+// Creación de pinia, para los stores
 export const pinia = createPinia()
+
+// Exportar stores para fácil acceso
+export { useSyncStore } from './syncStore.js'
+export { useImageStore } from './imageStore.js'
+export { useStationStore } from './stationStore.js'
+export { useSurveyStore } from './surveyStore.js'
