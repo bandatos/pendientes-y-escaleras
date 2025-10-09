6c828564ec60e32b6814986bef07a711cdd0d821
Seldon <daanmaro99june@gmail.com>
2025-10-08 19:25:40 -0600

    [vue] feat(MessageSnackBar): add new component for displaying snack bar notifications

---
+++ vue/src/components/MessageSnackBar.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { watch } from "vue";
+
+defineProps({
+  show: {
+    type: Boolean,
+    default: false,
+  },
+  text: {
+    text: String,
+    required: true,
+    default: "",
+  },
+  timer: {
+    type: String,
+    default: "5000",
+  },
+  type: {
+    type: String,
+  },
+});
+
+const showSnackBar = ref(false);
+
+watch(())
+
+</script>
+<template>
+  <v-snackbar
+    v-model="showSnackBar"
+    :color="type"
+    :timeout="timer != '0' && timer != '' ? timer : '-1'"
+    elevation="10"
+    :max-width="1000"
+  >
+    <template v-slot:action="{ attrs }">
+      <v-btn
+        light
+        v-bind="attrs"
+        @click="showSnackBar = false"
+        class="text-caption"
+      >
+        <span v-if="type == 'success'">OK</span>
+        <span v-else-if="type == 'warning'">ENTENDIDO</span>
+        <span v-else>ACEPTAR</span>
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+<style scoped>
+</style>
