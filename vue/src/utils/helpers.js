30f17b5bccd2e1ad76048b8d3b9e4a6cfeb8cfe4
Seldon <daanmaro99june@gmail.com>
2025-10-20 20:02:10 -0600

    [vue] feat(auth): Implement authHeader function to manage authorization headers with JWT token

---
+++ vue/src/utils/helpers.js
@@ -0,0 +1,28 @@
+import { LocalStorageService } from '../services/localStorage.js'
+const storage = LocalStorageService;
+
+export function authHeader(isFormData) {
+    // return authorization header with jwt token
+
+    if (isAuthValid()) {
+        let token = storage.getToken();
+
+        if (token) {
+            if (!isFormData) {
+                return {
+                    'Authorization': 'Token' + token,
+                    'Content-Type': 'application/json'
+                };
+            } else {
+                /* Si se manda argumento, entonces no es tipo JSON */
+                return {
+                    'Authorization': 'Token' + token,
+                };
+            }
+        } else {
+            /* storage.logout(); */
+
+            location.reload(true);
+        }
+    }
+}
